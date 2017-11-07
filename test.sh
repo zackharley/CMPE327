064652eeaaee4094e8180b215e4e4bdb2c4292f9
Zack Harley <zackharley@gmail.com>
2017-11-06 21:09:12 -0500

    Add storing logs

--- test.sh
+++ test.sh
@@ -16,8 +16,22 @@ case ${key} in
     shift # past argument
     shift # past value
     ;;
+    -o|--output)
+    OUTPUT="$2"
+    shift # past argument
+    shift # past value
+    ;;
 esac
 done
+
+TIMESTAMP=$(date "+%Y.%m.%d-%H.%M.%S")
+
+if ! [ ${OUTPUT} ]; then
+    OUTPUT="frontend/test/logs/test.${TIMESTAMP}.log"
+fi
+
+echo "TEST SESSION -- ${TIMESTAMP}" > ${OUTPUT}
+echo "" >> ${OUTPUT}
 
 if ! [[ ${FRONTEND} ||  ${BACKEND} ]]; then
     echo "You must supply a path to at least a frontend or backend test folder"
@@ -28,10 +42,10 @@ if [ ${FRONTEND} ]; then
     for folder in $( ls ${FRONTEND} ); do
         for file in $( find ${FRONTEND}/${folder} -name "*.input.txt" -print0 | xargs -0 ls); do
 
-            echo "######  Running test ${file}  ######"
+            echo "######  Running test ${file}  ######" >> ${OUTPUT}
             ACCOUNTS_FILE=$(echo ${file} | awk -F'[.]' '{print $1}' ).accounts.txt
-            python3 -m frontend ${ACCOUNTS_FILE} ${file}
-            echo ""
+            python3 -m frontend ${ACCOUNTS_FILE} ${file}  >> ${OUTPUT} 2>&1
+            echo "" >> ${OUTPUT}
         done
     done
 fi
