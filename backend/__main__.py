d17a85ff7fcad5ee34a2b31078555ff8337cdcdd
ryanmahjour <rmahjour@rogers.com>
2017-11-23 20:55:40 -0500

    small changes to naming

--- backend/__main__.py
+++ backend/__main__.py
@@ -6,10 +6,10 @@ def main():
     backend = Backend()
     if True: # If test mode
         test_harness = TestHarness()
-        test_harness.test_create_account("createacct", "1234567", "Rick")
-        #test_harness.test_create_account("ggg", "1234567", "Rick")
-        #test_harness.test_create_account("deposit", "1234567", "Rick")
-        #test_harness.test_create_account("createacct", "0000000", "Rick")
+        test_harness.test_create_account("createacct", "1234567", "JOHN")
+        #test_harness.test_create_account("ggg", "1234567", "JOHN")
+        #test_harness.test_create_account("deposit", "1234567", "JOHN")
+        #test_harness.test_create_account("createacct", "0000000", "JOHN")
 
 
 
